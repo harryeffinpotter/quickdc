ca4420d84b45894169fa5f44663ebe1478e264c8
harryeffinpotter <thisisyourknife@gmail.com>
2026-01-01 01:02:10 -0500

    First commit

--- reset-wifi.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File \"%~dp0reset-wifi.ps1\"' -Verb RunAs"
--- reset-wifi.ps1
+++
@@ -1,4 +0,0 @@
-# Reset WiFi - Disable, wait 5 seconds, then re-enable
-Disable-NetAdapter -Name "Wi-Fi 6" -Confirm:$false
-Start-Sleep -Seconds 5
-Enable-NetAdapter -Name "Wi-Fi 6" -Confirm:$false
